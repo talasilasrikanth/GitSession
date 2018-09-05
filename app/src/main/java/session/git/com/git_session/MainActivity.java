f25b2e4f730e45f7302cf317be4c03c06a2e135c
srikanth <talasilasrikanth51@gmail.com>
2018-09-05 15:26:54 +0530

    Added new Log Prod

--- app/src/main/java/session/git/com/git_session/MainActivity.java
+++ app/src/main/java/session/git/com/git_session/MainActivity.java
@@ -14,5 +14,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Log.v(TAG, "onCreate");
         Log.v(TAG, "Dev");
+        Log.v(TAG, "Prod");
     }
 }
